d85f262d32d36024cddc4e1027e0b224632388ff
Paul <Paul@INM-TB08.uni-stuttgart.de>
2024-03-27 11:23:46 +0100

    changed the name of the nonlincon function to be more specific about the type of model it belongs to, namely a 3parameter (fractional) linear model

--- nonlincon_3parLinear.m
+++ nonlincon_3parLinear.m
@@ -1,4 +1,4 @@
-function [c,ceq] = nonlincon_identification(p)
+function [c,ceq] = nonlincon_3parLinear(p)
 % nonlinear constraint to the normalized parameters b, c, d for them to
 % correspond to physical parameters.
 c = p(4)/p(2)-p(3);     % Compute nonlinear inequalities at p.
